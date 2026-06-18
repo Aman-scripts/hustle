8a2169348541591abb8e4808ee26d1026a182160
Amandeep <amandeep.nugen@gmail.com>
2026-06-18 17:50:41 +0530

    api structure

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+const express = require("express");
+const app = express();
+const port = 8088;
+
+app.get("",(req,res)=>{
+    res.send("Hello World! from backend")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("Hello World! from about page")
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
+
